9e6baf8ed931ada05be4c58e022646a2a9819d16
lm <godhestia0@gmail.com>
2023-03-16 11:22:52 +0100

    Repaire

---
+++ Access/Models/Moyenne.cs
@@ -0,0 +1,14 @@
+﻿
+
+using DataAccess;
+
+namespace MODELES
+{
+    internal class Moyenne : Model
+    {
+        public string id_elve = ""; 
+        public string id_filiere = "";
+        public string niveau = "";
+        public float moyenne = 0;
+    }
+}
